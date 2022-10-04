477a2ab4a89007b9fe636f4c8eaa66f74ef7a7db
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-03 20:59:29 -0700

    Milo Dominguez

--- q2.py
+++ q2.py
@@ -8,3 +8,15 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+
+user_string = input()
+string_max = user_string
+string_min = user_string
+while user_string != "Stop" and user_string != "stop":
+    if len(user_string) > len(string_max):
+        string_max = user_string
+    if len(user_string) < len(string_min):
+        string_min = user_string
+    user_string = input()
+
+print(f"{string_max} {string_min}")
