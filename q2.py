57a6cdb6e36f69daedf095b408ad87406cbfc33f
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-03 21:07:09 -0700

    Milo Dominguez

--- q2.py
+++ q2.py
@@ -1,18 +1,7 @@
-
-# ******************************
-# Make your Code
-# ******************************
-
-
-# Requirement
-# No need to use list
-# All input values are taken one by one separatively.
-###
-
 user_string = input()
 string_max = user_string
 string_min = user_string
-while user_string != "Stop" and user_string != "stop":
+while user_string != "STOP" and user_string != "stop":
     if len(user_string) > len(string_max):
         string_max = user_string
     if len(user_string) < len(string_min):
